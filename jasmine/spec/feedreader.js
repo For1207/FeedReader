6e53e1a22a4c0d73057cf0828ddfc21895063ae4
For1207 <53959211+For1207@users.noreply.github.com>
2019-10-04 19:10:26 -0400

    feat: First test function added

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,12 +26,13 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('url defined', function() {
 
+         });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
